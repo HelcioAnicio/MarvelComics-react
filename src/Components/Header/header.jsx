2f04fcef6217ea4382dc6c113e6b0d38b858cfcc
Helcio <helcioanicio19@gmail.com>
2023-10-11 23:01:30 -0300

    Update component Header, but still contain something wrong

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import LogoMarvel from '../../assets/logo.png'
 import {GiHamburgerMenu} from 'react-icons/gi'
+import { FaRegTimesCircle } from "react-icons/fa";
 
 export function Header () {
     return (
@@ -8,14 +9,19 @@ export function Header () {
             <header className='flex items-center justify-between px-4'>
                 <img src={LogoMarvel} alt="Logo Marvel" className='w-20'/>
                 <button type="button" className="bg-transparent">
-                    <GiHamburgerMenu className='text-4xl p-1 rounded text-black'  />
+                    <GiHamburgerMenu className='text-4xl text-black'  />
                 </button>
                 <nav className="absolute bg-slate-900 text-white w-full h-full top-0 right-0 flex items-center justify-center">
-                    <ul className="w-1/2 h-1/2 border-white border-solid border-2 flex flex-wrap gap-10 justify-center content-evenly">
-                        <li>Home</li>
-                        <li>Characters</li>
-                        <li>Comics</li>
-                        <li>Contact</li>
+                    <ul className="w-1/2 h-1/2 border-white border-solid border-2 flex-col flex-wrap justify-between items-center">
+                        <li className='text-right'> 
+                            <button type="button">
+                                <FaRegTimesCircle className='text-3xl mb-5'/>
+                            </button>
+                        </li>
+                        <li className='inline-block border border-w'>Home</li>
+                        <li className='inline-block border border-w'>Characters</li>
+                        <li className='inline-block border border-w'>Comics</li>
+                        <li className='inline-block border border-w'>Contact</li>
                     </ul>
                 </nav>
             </header>   
